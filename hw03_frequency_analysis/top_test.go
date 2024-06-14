84d3f240fa688d173b102820c842531dc2a9bfc5
Rudeshko Nikita Andreevich <nrudeshko@ozon.ru>
2024-06-14 21:48:09 +0300

    solve task

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -3,11 +3,12 @@ package hw03frequencyanalysis
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -79,4 +80,191 @@ func TestTop10(t *testing.T) {
 			require.Equal(t, expected, Top10(text))
 		}
 	})
+}
+
+func Test_getWordStats(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want map[string]int
+	}{
+		{
+			name: "all words are different",
+			text: "жила-была алиса в стране чудес",
+			want: map[string]int{
+				"жила-была": 1,
+				"алиса":     1,
+				"в":         1,
+				"стране":    1,
+				"чудес":     1,
+			},
+		},
+		{
+			name: "using non-similar words",
+			text: "тест Тест тест, тест тест тест, Тест, тест Тест тест2",
+			want: map[string]int{
+				"тест":  9,
+				"тест2": 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, getWordStats(tt.text))
+		})
+	}
+}
+
+func Test_topKeys(t *testing.T) {
+	type args struct {
+		m     map[string]int
+		count int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "all words with same count",
+			args: args{
+				m: map[string]int{
+					"жила-была": 1,
+					"алиса":     1,
+					"в":         1,
+					"стране":    1,
+					"чудес":     1,
+				},
+				count: 5,
+			},
+			want: []string{
+				"алиса",
+				"в",
+				"жила-была",
+				"стране",
+				"чудес",
+			},
+		},
+		{
+			name: "some words with same count, some words with different",
+			args: args{
+				m: map[string]int{
+					"аист":  2,
+					"чайка": 3,
+					"кошка": 3,
+					"робот": 1,
+					"целый": 1,
+				},
+				count: 5,
+			},
+			want: []string{
+				"кошка",
+				"чайка",
+				"аист",
+				"робот",
+				"целый",
+			},
+		},
+		{
+			name: "less words than map length",
+			args: args{
+				m: map[string]int{
+					"аист":  2,
+					"чайка": 3,
+					"кошка": 3,
+					"робот": 1,
+					"целый": 1,
+				},
+				count: 2,
+			},
+			want: []string{
+				"кошка",
+				"чайка",
+			},
+		},
+		{
+			name: "count more than map length",
+			args: args{
+				m: map[string]int{
+					"аист":  2,
+					"чайка": 3,
+					"кошка": 3,
+					"робот": 1,
+					"целый": 1,
+				},
+				count: 10,
+			},
+			want: []string{
+				"кошка",
+				"чайка",
+				"аист",
+				"робот",
+				"целый",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, topKeys(tt.args.m, tt.args.count))
+		})
+	}
+}
+
+func Test_normalizeWord(t *testing.T) {
+	type args struct {
+		word string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "word without punctuation marks",
+			args: args{
+				word: "hello",
+			},
+			want: "hello",
+		},
+		{
+			name: "word with commas",
+			args: args{
+				word: ",hello,,,",
+			},
+			want: "hello",
+		},
+		{
+			name: "word with dot and commas",
+			args: args{
+				word: ".hello,,,",
+			},
+			want: "hello",
+		},
+		{
+			name: "word is -",
+			args: args{
+				word: "-",
+			},
+			want: "",
+		},
+		{
+			name: "word is ----",
+			args: args{
+				word: "----",
+			},
+			want: "----",
+		},
+		{
+			name: "word with middle - ",
+			args: args{
+				word: ",one-two..-",
+			},
+			want: "one-two",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, normalizeWord(tt.args.word), "normalizeWord(%v)", tt.args.word)
+		})
+	}
 }
