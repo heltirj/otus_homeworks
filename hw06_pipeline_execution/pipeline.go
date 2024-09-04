a49876f8333e6e77514f66066e24274ddbd1380d
Rudeshko Nikita Andreevich <nrudeshko@ozon.ru>
2024-09-04 11:21:42 +0300

    implement the pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	res := make(Bi)
+
+	go func() {
+		out := CreatePipeline(in, stages...)
+		defer close(res) 
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-out:
+				if !ok {
+					return
+				}
+				res <- v
+			}
+		}
+	}()
+
+	return res
+}
+
+func CreatePipeline(in In, stages ...Stage) Out {
+	if len(stages) == 0 {
+		return nil
+	}
+
+	if len(stages) == 1 {
+		return stages[0](in)
+	}
+
+	return CreatePipeline(stages[0](in), stages[1:]...)
 }
